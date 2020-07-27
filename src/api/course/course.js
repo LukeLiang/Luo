2afeb2dd6232682ebf9947d6a0c41555f00b2ea9
LukeLiang <39796629+LukeLiang@users.noreply.github.com>
2020-07-27 18:23:32 +0800

    Add files via upload

---
+++ src/api/course/course.js
@@ -0,0 +1,23 @@
+const chooseCourses = () => {
+  return [
+    {
+      img: 'http://www.liangku.pro/static/o1.png',
+      title: '学生英语',
+      description: '零基础，从基础发音、词汇、语法开始学习，认识英语，会说英语，初步掌握英语简单交流。'
+    },
+    {
+      img: 'http://www.liangku.pro/static/o1.png',
+      title: '职业英语',
+      description: '英语口语，地道实用，轻松对话交流。涵盖工作、生活、娱乐等方方面面，告别哑巴英语。'
+    },
+    {
+      img: 'http://www.liangku.pro/static/o1.png',
+      title: '旅游英语',
+      description: '英语不行，出国寸步难行，旅游英语速成班助你顺畅自由行，自信出国，潇洒旅行。'
+    }
+  ]
+}
+
+export {
+  chooseCourses
+}
---
+++ src/api/teahcer/teacher.js
@@ -0,0 +1,28 @@
+const teacherList = () => {
+  return [
+    {
+      name: 'Bonnie',
+      profile: 'http://www.liangku.pro/static/m1.jpg',
+      info: 'Bonnie来自美国的一名职业教育家，拥有十余年的英语教学经验，曾在多国教过英语。'
+    },
+    {
+      name: 'Bonnie',
+      profile: 'http://www.liangku.pro/static/m1.jpg',
+      info: 'Daniel来自美国的一名职业教育家，拥有十余年的英语教学经验，曾在多国教过英语。'
+    },
+    {
+      name: 'Daniel',
+      profile: 'http://www.liangku.pro/static/m1.jpg',
+      info: 'Daniel来自美国的一名职业教育家，拥有十余年的英语教学经验，曾在多国教过英语。'
+    },
+    {
+      name: 'Daniel',
+      profile: 'http://www.liangku.pro/static/m1.jpg',
+      info: 'Daniel来自美国的一名职业教育家，拥有十余年的英语教学经验，曾在多国教过英语。'
+    }
+  ]
+}
+
+export {
+  teacherList
+}
